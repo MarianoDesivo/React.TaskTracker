8ffa569eed8d47d6f28456fd80a8a481512d6f5f
MarianoDesivo <marianodesivo96@gmail.com>
2023-02-06 17:30:30 -0300

    Added json-server

--- src/App.js
+++ src/App.js
@@ -1,67 +1,91 @@
 //import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom'
 import './style.css';
 import Header from './components/Header.js';
 import Tasks from './components/Tasks.js';
 import AddTask from './components/AddTask.js';
+import Footer from './components/Footer.js'
+import About from './components/About.js'
 
 export default function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'task1',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'task2',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false,
-    },
-    {
-      id: 3,
-      text: 'task3',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 4,
-      text: 'task4',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-  ]);
+    const [tasks, setTasks] = useState([
+        {
+            "id": 1,
+            "text": "Cut lawn",
+            "day": "Feb 5th at 2:30pm",
+            "reminder": true
+        },
+        {
+            "id": 2,
+            "text": "task2",
+            "day": "Feb 5th at 2:30pm",
+            "reminder": false
+        }
+    ]);
+    
+    useEffect(() => {  
+        setTasksFromServer();
+    }, [])
 
+    const setTasksFromServer = async () => {
+        const tasksFromServer = await fetchTasks();
+        setTasks(tasksFromServer);
+    }
+  const fetchTasks = async () => {
+     const res = await fetch("http://localhost:5000/tasks");
+     const data = await res.json();
+
+     console.log(data);
+     return data;
+    }
+    const fetchTask = async (id) => {
+        const res = await fetch(`http://localhost:5000/tasks/${id}`);
+        const data = await res.json();
+
+        console.log(data);
+        return data;
+    }
   const toggleAddTask = () => {
     setShowAddTask(!showAddTask);
   };
-  const onAdd = (newTask) => {
-    newTask.id = tasks.length + 1;
+  const onAdd = async (newTask) => {
     console.log('Creating new task:', newTask);
-    setTasks([...tasks, newTask]);
+      await fetch(`http://localhost:5000/tasks/`, {
+          method: 'POST',
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(newTask)
+      })
+      setTasksFromServer();
   };
 
-  const deleteTask = (id) => {
-    console.log('delete ' + id);
-    setTasks(tasks.filter((task) => task.id !== id));
+  const deleteTask = async (id) => {
+      console.log('delete ' + id);
+      await fetch(`http://localhost:5000/tasks/${id}`, {
+          method: 'DELETE'
+      })
+      setTasksFromServer();
   };
 
-  const toggleReminder = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
-      )
-    );
+  const toggleReminder =async (id) => {
+      const taskToToggle = await fetchTask(id);
+      taskToToggle.reminder = !taskToToggle.reminder;
+      await fetch(`http://localhost:5000/tasks/${id}`, {
+          method: 'PUT',
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(taskToToggle)
+      })
+
+      setTasksFromServer();
     console.log(
       'reminder',
-      id,
-      tasks.filter((task) => task.id === id)[0].reminder
+        id,
+        taskToToggle.reminder
     );
   };
 
-  return (
+    return (
     <div className="container">
       <Header
         onClick={toggleAddTask}
@@ -78,6 +102,8 @@ export default function App() {
       ) : (
         'There are no Tasks, please add one.'
       )}
-    </div>
+
+       <Footer />    
+       </div>     
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h4>Version 1.0.0 by Mariano Desivo</h4>
+            <a href="/">Go back</a>
+      </div>
+    
+  );
+}
+
+export default About;
